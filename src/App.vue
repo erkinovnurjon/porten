359ad8711d5216ad27c6bdc9088ca2dd0cc761db
Nurjon <nurjon2477@gmail.com>
2023-11-20 00:30:29 +0500

    vue project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template >
+  <div class="container">
+    <Header />
+    <Navbar />
+    <Hero />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Hero from './components/Hero.vue';
+import Navbar from './components/Navbar.vue';
+export default {
+  components:{
+    Header,
+    Navbar,
+    Hero
+}
+}
+</script>
+
+<style >
+  .container{
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 16px;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template >
+      <div>
+            <img src="./../assets/phone.svg" alt="phone">
+            <span>8 (812) 123-45-67    |    Работаем 7 дней в неделю    |    9:00 — 18:00  </span>
+            <img src="./../assets/exit.svg" alt="">
+      </div>
+</template>
+<script>
+export default {
+      
+}
+</script>
+<style >
+      
+</style>
